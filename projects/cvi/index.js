ff6da5bcd2eaacfc7687d1133b3005ce89a9abaa
Vlad Piliavsky <vladi@coti.io>
2021-03-22 13:45:38 +0200

    Added CVI

---
+++ projects/cvi/index.js
@@ -0,0 +1,54 @@
+/*==================================================
+  Modules
+  ==================================================*/
+
+const sdk = require("../../sdk");
+
+/*==================================================
+  Settings
+  ==================================================*/
+
+const USDTPlatform = "0xe0437BeB5bb7Cf980e90983f6029033d710bd1da"; // CVI USDT Platform Contract
+const ETHPlatform = "0x145e6df857b241efdcb9b78a1b97a032606d47a7"; // CVI ETH Platform Contract
+
+const USDT = "0xdac17f958d2ee523a2206206994597c13d831ec7";
+const GOVI = "0xeeaa40b28a2d1b0b08f6f97bb1dd4b75316c6107";
+
+/*==================================================
+  TVL
+  ==================================================*/
+
+async function tvl(timestamp, block) {
+  const USDTRequest = sdk.api.erc20.balanceOf({
+    target: USDT,
+    owner: USDTPlatform,
+    block: block,
+  });
+
+  const ETHRequest = sdk.api.eth.getBalance({
+    block,
+    target: ETHPlatform,
+  });
+
+  const [USDTResponse, ETHResponse] = await Promise.all([
+    USDTRequest,
+    ETHRequest,
+  ]);
+
+  return {
+    [USDTPlatform]: USDTResponse.output,
+    [ETHPlatform]: ETHResponse.output,
+  };
+}
+/*==================================================
+  Exports
+  ==================================================*/
+
+module.exports = {
+  name: "CVI",
+  website: "https://cvi.finance/",
+  token: "GOVI",
+  category: "trading",
+  start: 1611073144, // Jan-19-2021 04:19:04 PM +UTC
+  tvl,
+};
